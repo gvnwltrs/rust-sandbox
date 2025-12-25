9ffef161675d471f5298642b60eaba6fe725a702
Gavin <waltersg298@gmail.com>
2025-12-25 13:30:56 -0700

    updated comments and console output

--- rust-catalog/src/main.rs
+++ rust-catalog/src/main.rs
@@ -3,6 +3,7 @@ use content::media::{Media, describe_into, create_buf};
 use content::catalog::Catalog;
 
 fn main() {
+    // Setup catalog data and container 
     let book = Media::new_book("The Art of War", "Sun Tzu");
     let audiobook = Media::new_audiobook("The 48 Laws of Power");
     let movie = Media::new_movie("Batman", "Christopher Nolan");
@@ -13,11 +14,13 @@ fn main() {
     // Check libary created 
     if library.is_empty() { println!("Library exists."); }
 
+    // Display raw media data 
     book.print_media();
     audiobook.print_media();
     movie.print_media();
     podcast.print_media();
 
+    // Add a new media to library
     library.add(book);
     query = library.get_by_index(0);
     println!("Media query: {:?}", query);
@@ -26,11 +29,12 @@ fn main() {
     query = library.get_by_index(1);
     println!("Media query: {:?}", query);
 
+    // Get libarary size
     println!("Size of library: {:?}", library.len());
 
-    // Get a description
+    // Get a description of media
     let mut buf = create_buf::<64>();
     describe_into(&audiobook, &mut buf);
-    println!("Audiobook description: {:?}", buf);
+    println!("Description: {:?}", buf);
 
 }
