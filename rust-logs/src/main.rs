31cbb01cb76f40906b8001ebbd35a32b51aadee0
Gavin Walters <gavin@gavinwalters.com>
2025-12-26 11:29:32 -0700

    feature: added error extraction for logs

--- rust-logs/src/main.rs
+++ rust-logs/src/main.rs
@@ -1,9 +1,36 @@
 use std::fs;
+use std::io::Error;
+
+/* 
+function extract_errors(log: string) -> list of strings: 
+    split log by newline characters into lines
+    initialize an empty list called result
+
+    for each line in lines:
+        if line starts with "ERROR": 
+            add the line to the results list
+
+    return result list
+*/
+fn extract_errors(logs: &String) -> Result<Vec<String>, Error> {
+    let mut logged_errors = Vec::new();
+    for line in logs.lines() {
+        if line.starts_with("ERROR") {
+            logged_errors.push(line.into());
+        }
+    }
+    Ok(logged_errors)
+}
 
 fn main() {
+    let mut file = String::new();
+
     // TODO: reading into a heap-based String; change to stack
     match fs::read_to_string("logs.txt")  {
-        Ok(contents) => println!("SUCCESS: file read -- {:#?}", contents), 
+        Ok(contents) => {
+            println!("SUCCESS: file read -- {:#?}", contents);
+            file = contents.into();
+        }
         Err(failure) => {
             println!("ERROR: {:#?}", failure); 
         } 
@@ -14,5 +41,10 @@ fn main() {
         Err(failure) => {
             println!("ERROR: {:#?}", failure); 
         } 
+    }
+
+    match extract_errors(&file) {
+        Ok(errors) => println!("SUCCESS: errors extracted -- {:#?}", errors),
+        Err(failure) => println!("ERROR: {:#?}", failure),
     }
 }
