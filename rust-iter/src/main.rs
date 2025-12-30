e0b0b3371eb3fe07d56e78848624239daa7209f3
Gavin Walters <gavin@gavinwalters.com>
2025-12-29 18:01:29 -0700

    feature: implemented variants for iterators

--- rust-iter/src/main.rs
+++ rust-iter/src/main.rs
@@ -1,13 +1,44 @@
 
 
 fn print_elements(elements: &Vec<String>) {
-    let mut iter = elements.iter();
-    loop {
-        match iter.next() {
-            Some(cur) => println!("{:#?}", cur),
-            None => break, 
-        }
-    }
+
+    // Instructor's version:
+    //
+    // for element in elements {
+    //      println!("{}", element);  
+    //}
+    //
+    // Below is what's actually happening under the hood:
+
+    // My version: 
+    // let mut iter = elements.iter();
+    // loop {
+    //     match iter.next() {
+    //         Some(cur) => println!("{:#?}", cur),
+    //         None => break, 
+    //     }
+    // }
+
+    // Version without using .next()
+    // elements.iter().for_each(|el| println!("{:#?}", el));
+
+    // Versions demonstrating iterator adapters
+    // let map = elements.iter().map(|item| format!("{:#?}", item));
+    // map.for_each(|el| println!("{:#?}", el));
+
+    // Using map and for_each
+    elements.iter().map(|x| format!("{}", x)).for_each(|x| println!("{}", x));
+
+    // Using filter and for_each
+    elements.iter().filter(|x| *x == "green").for_each(|x| println!("{}", x));
+
+    // Using map, filter, and for_each
+    elements
+        .iter()
+        .filter(|x| x.as_str() == "green")
+        .map(|x| format!("found it: {}", x) )
+        .for_each(|x| println!("{}", x));
+
 }
 
 #[cfg(test)]
