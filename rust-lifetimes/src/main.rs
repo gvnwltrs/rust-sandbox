f9aef9190618ee9972627a32466c83a55cec7e7c
Gavin Walters <gavin@gavinwalters.com>
2026-01-01 10:08:37 -0700

    feature: added get last language

--- rust-lifetimes/src/main.rs
+++ rust-lifetimes/src/main.rs
@@ -1,10 +1,12 @@
 
-fn next_language<'a>(languages: &'a [String], current: &str) -> Option<&'a str> {
+fn next_language<'a>(languages: &'a [String], current: &str) -> &'a str {
     let mut found = false;
 
+    // Cycle through the languages until you land on a match, and return
+    // the one right after the match if there is one...
     for lang in languages {
         if found {
-            return Some(lang.as_str());
+            return lang.as_str();
         }
 
         if lang == current {
@@ -12,15 +14,34 @@ fn next_language<'a>(languages: &'a [String], current: &str) -> Option<&'a str> 
         }
     }
 
-    Some(languages.last().unwrap().as_str())
+    // If no match is found or nothing comes after the match, just return
+    // something, or in this case, just return the very last language available
+    languages.last().unwrap().as_str()
 }
 
+// Requires lifetime annotation because we have two ref inputs and 
+// don't know what one is getting returned
 fn next_language_v2<'a>(languages: &'a [String], current: &str) -> Option<&'a str> {
     languages
         .iter()
         .position(|s| s == current)
         .and_then(|i| languages.get(i+1))
         .map(|s| s.as_str())
+}
+
+// Does not require lifetime annotation because we only have one 
+// input reference and can safely assume it's the only one being returned
+// either fully or as a slice in this case (points back to the oringal)
+fn last_language(languages: &[String]) -> &str {
+    let last = languages
+        .iter()
+        .last();
+
+    match last {
+        Some(s) => s.as_str(),
+        None => "NOTHING"
+    }
+    // "NOTHING" // living in .rodata somewhere
 }
 
 #[cfg(test)]
@@ -36,10 +57,25 @@ mod tests {
         ];
 
         // try next langauge 
-        let cur_lang = next_language(&languages, "rust");
+        let cur_lang = next_language_v2(&languages, "rust");
         let expected = "go";
 
         assert_eq!(cur_lang.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_last_language() {
+        let languages = vec![
+            String::from("rust"),
+            String::from("go"),
+            String::from("typescript"),
+        ];
+
+        let last_lang = last_language(&languages);
+        let expected = "typescript";
+
+        assert_eq!(last_lang, expected);
+
     }
 }
 
@@ -52,11 +88,21 @@ fn main() {
         String::from("typescript"),
     ];
 
-    println!("Current language: {:#?}",languages.get(0).unwrap());
-    let language = next_language(&languages, languages.get(0).unwrap());
-    println!("Next language: {:#?}", language.unwrap());
+    let mut language = match languages.get(0) {
+        Some(s) => s,
+        None => "NONE",
+    };
 
-    println!("Current language: {:#?}", language.unwrap());
-    let language = next_language_v2(&languages, language.unwrap());
-    println!("Next language: {:#?}", language.unwrap());
+    println!("Current language: {:#?}", language);
+    language = next_language(&languages, &language); 
+    println!("Next language: {:#?}", language);
+
+    println!("Current language: {:#?}", language);
+    language = match next_language_v2(&languages, &language) {
+        Some(s) => s,
+        None => "NONE",
+    };
+    println!("Next language: {:#?}", language);
+
+    println!("Last language: {:#?}", last_language(&languages)); 
 }
