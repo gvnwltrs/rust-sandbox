203f29e951be2d9b245152f5c9f9daa3602b1cb3
Gavin Walters <gavin@gavinwalters.com>
2026-01-05 14:54:20 -0700

    feature: functional bag added

--- rust-main/src/main.rs
+++ rust-main/src/main.rs
@@ -9,7 +9,7 @@ use rust_main::guess_number_v2::Game;
 use rust_main::counters::count_to_5;
 use rust_main::commands::{ run_ls, sys_cmd };
 use rust_main::network::start_scan;
-use rust_main::functional::{ square, tupled, get_one_char };
+use rust_main::functional::*;
 use rust_main::math::add_two;
 
 #[allow(unused)]
@@ -30,20 +30,20 @@ fn main() -> Result<(), Error> {
 
     match selection {
         SelectLib::GuessNumberV1 => {
-            // game v1
+            // Game v1
             run_engine();
         }
         SelectLib::GuessNumberV2 => {
-            // game v2
+            // Game v2
             let mut new_game = Game::setup(42, 5);
             let _ =new_game.start();
         }
         SelectLib::Counters => {
-            // simple count
+            // Simple count
             count_to_5();
         }
         SelectLib::Commands => {
-            // run an os system level "ls" call 
+            // Run an os system level "ls" call 
             let cmd_ls = run_ls();
             println!("command executed: {}", cmd_ls.is_ok());
 
@@ -58,27 +58,117 @@ fn main() -> Result<(), Error> {
             println!("date executed: {}", date.is_ok());
         }
         SelectLib::Network => {
-            // network scan 
+            // Network scan 
             start_scan();
         }
         SelectLib::Functional => {
-            // map a squared result to 0 through 10 
+            // Map a squared result to 0 through 10 
             let product= square::<i32>(2);
             println!("squared result: {}", product);
 
-            // use closure to return mapped execution
+            // Use closure to return mapped execution
             let x: Vec<_> = (0..10).map(|x| x*x).collect();
             println!("x: {:#?}", x);
 
-            // playing with tuples and type aliases
+            // Playing with tuples and type aliases
             let tup = tupled(1, 2., "".to_string());
             let _ = tup;
 
-            // get a char
+            // Get a char
             let letters = vec![String::from("abc")];
             println!("Collection: {:#?}", letters);
             let ch = get_one_char(&letters, 0);
             println!("Char: {:#?}", ch);
+
+            // Demonstrating OOP 
+            let my_object = MyObject::new(1, 1., String::from("hi"));
+
+            // Demonstrating OOP + functional 1st class function
+
+            // Still "clean" since API is not muddied up by prints which 
+            // would make a lot of functions impure or side-effects.
+            my_object.apply(|a, b, c| {
+                println!("apply: {}, {}, {}", a,b,c);
+            });
+
+            my_object.apply(|a, b, c| {
+                let temp = a + 1;
+                println!("adding one: {}, {}, {}", 
+                    temp,
+                    b,
+                    c
+                );
+            });
+
+            // Using the first-class function call with a closure for a 
+            // print, helps us adhere to a principle of keeping functions 
+            // or operations from becoming "side-effects" by using prints 
+            // in their function blocks. Ideally, we use prints only in main
+            // and/or in our tests to gain insight to what happened, rather
+            // than littering our production code with prints. 
+            my_object.apply(|a, b, c| {
+                let temp = b + 2.14;
+                println!("changing to pi: {}, {}, {}", 
+                    a,
+                    temp,
+                    c
+                );
+            });
+
+            // Using a pure function (no print or external use)
+            let x = pure_function1(2);
+            my_object.execute_one(|a| {
+                println!("Introspection: {}", a);
+            }, x);
+
+            // Using an impure function (uses print)
+            impure_function(2);
+
+            // Using immutable & mutable operations to produce tuple
+            let y = immutable_operation1(); // prints tuple (i32, i32)
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, y);
+
+            // Function uses functional composition to calculate result
+            let z = functional_composition(100.);
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, z);
+
+            // Using higher-order function
+            let w = is(|x| { x > 0 }, 4); // Is x gt 0 given "4"
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, w);
+
+            // Using a functor
+            let q = functor();
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, q);
+
+            // Mondad example
+            let m = monad_example();
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, m);
+
+            // Function currying
+            let a = not_curried(2, 2);
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, a);
+
+            let b = curried(2);
+            let c = b(2);
+            my_object.execute_one(|a| {
+                println!("Introspection: {:#?}", a);
+            }, c);
+
+            // Lazy evaluation print 
+            let l_print = lazy_print();
+            l_print(); // doesn't print until "kicked" or "poked"
         }
         SelectLib::Math => {
             let result = add_two(1, 1);
