0d1b8a9febbbb18e7cbd8ad18adda090f67b27ed
Gavin Walters <gavin@gavinwalters.com>
2026-01-10 11:38:42 -0700

    feature: added some demo of using data structs and traits...

--- rust-functional/src/functional/mod.rs
+++ rust-functional/src/functional/mod.rs
@@ -1,18 +1,15 @@
-// pub mod functional;
-// pub use functional::*;
-
-pub fn my_func() { println!("this works");}
-
 /* 
 * Intro to Functional Programming in Rust 
 **/
+use serde::{Serialize,Deserialize};
+
+pub fn my_func() { println!("this works");}
 
 // This is a "pure function" because it does not modify the original, or
 // the input "x: T". It only use a read-only of "x" to perform a calculation
 // and returns the result of that calculation which is then owned by the caller.
 pub fn square<T>(x: T) -> T 
-where T: std::ops::Mul<Output=T> + Copy
-{  
+where T: std::ops::Mul<Output=T> + Copy {  
     x*x
 }
 
@@ -232,9 +229,8 @@ impl<T> Monad for Option<T> {
 
     fn bind<B, F>(self, f: F) -> Option<B>
     where
-        F: FnOnce(T) -> Option<B>,
-    {
-        self.and_then(f)
+        F: FnOnce(T) -> Option<B>, {
+            self.and_then(f)
     }
 }
 
@@ -338,7 +334,145 @@ pub fn macro_branching() {
     my_macro_branch!(2 "def");
 }
 
+/* 
+ * Functional data structures 
+ **/
+
+ // Enums are also known as "tagged unions". This allows us to define data
+ // that may have multiple structures or use cases. 
+ #[derive(Clone,Serialize,Deserialize,Debug)]
+ pub enum MotorInput {
+    Up { voltage: f64 },
+    Down { voltage: f64 },
+ }
+
+ // Note: trait items always share the visibility of their trait
+ // , so you don't have to use 'pub' in the impl in this case
+ pub trait MotorForce {
+    fn calculate_force(&self) -> f64; // contract for impl to implement
+ }
+
+ impl MotorForce for MotorInput {
+    fn calculate_force(&self) -> f64 {
+        match *self {
+            MotorInput::Up { voltage: v } => { v * 8.0 }
+            MotorInput::Down { voltage: v } => { v * -8.0 }
+        }
+    }
+ }
+
+ pub trait MotorVoltage {
+    fn voltage(&self) -> f64;
+ }
+
+impl MotorVoltage for MotorInput {
+    fn voltage(&self) -> f64 {
+        match *self {
+            MotorInput::Up { voltage: v } => { v }
+            MotorInput::Down { voltage: v } => { -v }
+        }
+    }
+}
+
+#[derive(Clone,Serialize,Deserialize,Debug)]
+pub struct ElevatorSpecification {
+    pub floor_count: u64,
+    pub floor_height: f64,
+    pub carriage_weight: f64,
+}
+
+#[derive(Clone,Serialize,Deserialize,Debug)]
+pub struct ElevatorState {
+    pub timestamp: f64,
+    pub location: f64,
+    pub velocity: f64,
+    pub acceleration: f64,
+    pub motor_input: MotorInput,
+}
+
+pub type FloorRequests = Vec<u64>;
+
+pub trait MotorController {
+    fn init(&mut self, esp: ElevatorSpecification, est: ElevatorState);
+    fn poll(&mut self, esp: ElevatorState, dst: u64);
+}   
+
+pub struct SimpleMotorController {
+    pub esp: ElevatorSpecification
+}
+
+#[allow(unused)]
+impl MotorController for SimpleMotorController {
+    fn init(&mut self, esp: ElevatorSpecification, est: ElevatorState) {
+        self.esp = esp;
+    }
+    fn poll(&mut self, esp: ElevatorState, dst: u64) {}
+}
+
+pub trait DataRecorder {
+    fn init(&mut self, esp: ElevatorSpecification, est: ElevatorState);
+    fn poll(&mut self, esp: ElevatorState, dst: u64);
+    fn summary(&mut self);
+}   
+
+#[derive(Debug)]
+#[allow(unused)]
+pub struct SimpleDataRecorder {
+    pub esp: ElevatorSpecification,
+    pub record_location: Vec<f64>,
+    pub record_velocity: Vec<f64>,
+    pub record_acceleration: Vec<f64>,
+    pub record_voltage: Vec<f64>,
+}
+
+#[allow(unused)]
+impl DataRecorder for SimpleDataRecorder {
+    fn init(&mut self, esp: ElevatorSpecification, est: ElevatorState) { 
+        self.esp = esp;
+    }
+    fn poll(&mut self, esp: ElevatorState, dst: u64) {}
+    fn summary(&mut self) {}
+}
+
+
+#[allow(unused)]
+pub fn simulate_elevator<'a, MC: MotorController, DR: DataRecorder>(
+    esp: ElevatorSpecification, // Copy/ref in 
+    est: ElevatorState, 
+    req: FloorRequests, 
+    mc: &mut MC, 
+    dr: &'a mut DR
+) -> &'a DR { // ref out as immutable
+    // Implements and modifies borrow-refs here...      
+
+    // Immutable input becomes mutable local state (defensive copies).
+    // Does not modify the external data. 
+    let mut esp = esp.clone(); // Copies esp input with a deep copy (own new copy)
+    let mut est = est.clone(); // (own new copy); does not take (est exists outside still)
+    let mut req = req.clone(); // (own new copy); does not take (req exists outside still) 
+    let floor_requests: FloorRequests = vec![1, 2, 3];
+
+    // Lazy implement here (not fully done, just demoing usages)
+    // Modifying only the copies here (zero out)
+    esp.floor_count = 0;
+    esp.floor_height = 0.0;
+    esp.carriage_weight = 0.0;
+    est.location = 0.0;
+    floor_requests.iter().for_each(|flr| req.push(*flr));
+    // Previous settings now modifed
+
+    // Initialize MotorController and DataController
+    mc.init(esp.clone(), est.clone()); // Copy into ref
+    dr.init(esp.clone(), est.clone()); // Copy into ref
+
+    dr
+}
+
+/* 
+ * Unit tests 
+ **/
 #[cfg(test)]
+#[allow(unused)]
 mod tests {
     use super::*;
 
--- rust-functional/src/main.rs
+++ rust-functional/src/main.rs
@@ -6,134 +6,155 @@ use std::io::Error;
 #[allow(unused_imports)]
 use rust_functional::functional::*;
 
-#[allow(unused)]
-enum SelectLib {
-    Functional,
-}
-
 fn main() -> Result<(), Error> {
-    println!("Rust Main Starting...");
 
-    let selection = SelectLib::Functional;
+    // Map a squared result to 0 through 10 
+    let product= square::<i32>(2);
+    println!("squared result: {}", product);
 
-    match selection {
-        SelectLib::Functional => {
-            // Map a squared result to 0 through 10 
-            let product= square::<i32>(2);
-            println!("squared result: {}", product);
+    // Use closure to return mapped execution
+    let x: Vec<_> = (0..10).map(|x| x*x).collect();
+    println!("x: {:#?}", x);
 
-            // Use closure to return mapped execution
-            let x: Vec<_> = (0..10).map(|x| x*x).collect();
-            println!("x: {:#?}", x);
+    // Playing with tuples and type aliases
+    let tup = tupled(1, 2., "".to_string());
+    let _ = tup;
 
-            // Playing with tuples and type aliases
-            let tup = tupled(1, 2., "".to_string());
-            let _ = tup;
+    // Get a char
+    let letters = vec![String::from("abc")];
+    println!("Collection: {:#?}", letters);
+    let ch = get_one_char(&letters, 0);
+    println!("Char: {:#?}", ch);
 
-            // Get a char
-            let letters = vec![String::from("abc")];
-            println!("Collection: {:#?}", letters);
-            let ch = get_one_char(&letters, 0);
-            println!("Char: {:#?}", ch);
+    // Demonstrating OOP 
+    let my_object = MyObject::new(1, 1., String::from("hi"));
 
-            // Demonstrating OOP 
-            let my_object = MyObject::new(1, 1., String::from("hi"));
+    // Demonstrating OOP + functional 1st class function
 
-            // Demonstrating OOP + functional 1st class function
+    // Still "clean" since API is not muddied up by prints which 
+    // would make a lot of functions impure or side-effects.
+    my_object.apply(|a, b, c| {
+        println!("apply: {}, {}, {}", a,b,c);
+    });
 
-            // Still "clean" since API is not muddied up by prints which 
-            // would make a lot of functions impure or side-effects.
-            my_object.apply(|a, b, c| {
-                println!("apply: {}, {}, {}", a,b,c);
-            });
+    my_object.apply(|a, b, c| {
+        let temp = a + 1;
+        println!("adding one: {}, {}, {}", 
+            temp,
+            b,
+            c
+        );
+    });
 
-            my_object.apply(|a, b, c| {
-                let temp = a + 1;
-                println!("adding one: {}, {}, {}", 
-                    temp,
-                    b,
-                    c
-                );
-            });
+    // Using the first-class function call with a closure for a 
+    // print, helps us adhere to a principle of keeping functions 
+    // or operations from becoming "side-effects" by using prints 
+    // in their function blocks. Ideally, we use prints only in main
+    // and/or in our tests to gain insight to what happened, rather
+    // than littering our production code with prints. 
+    my_object.apply(|a, b, c| {
+        let temp = b + 2.14;
+        println!("changing to pi: {}, {}, {}", 
+            a,
+            temp,
+            c
+        );
+    });
 
-            // Using the first-class function call with a closure for a 
-            // print, helps us adhere to a principle of keeping functions 
-            // or operations from becoming "side-effects" by using prints 
-            // in their function blocks. Ideally, we use prints only in main
-            // and/or in our tests to gain insight to what happened, rather
-            // than littering our production code with prints. 
-            my_object.apply(|a, b, c| {
-                let temp = b + 2.14;
-                println!("changing to pi: {}, {}, {}", 
-                    a,
-                    temp,
-                    c
-                );
-            });
+    // Using a pure function (no print or external use)
+    let x = pure_function1(2);
+    my_object.execute_one(|a| {
+        println!("Introspection: {}", a);
+    }, x);
 
-            // Using a pure function (no print or external use)
-            let x = pure_function1(2);
-            my_object.execute_one(|a| {
-                println!("Introspection: {}", a);
-            }, x);
+    // Using an impure function (uses print)
+    impure_function(2);
 
-            // Using an impure function (uses print)
-            impure_function(2);
+    // Using immutable & mutable operations to produce tuple
+    let y = immutable_operation1(); // prints tuple (i32, i32)
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, y);
 
-            // Using immutable & mutable operations to produce tuple
-            let y = immutable_operation1(); // prints tuple (i32, i32)
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, y);
+    // Function uses functional composition to calculate result
+    let z = functional_composition(100.);
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, z);
 
-            // Function uses functional composition to calculate result
-            let z = functional_composition(100.);
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, z);
+    // Using higher-order function
+    let w = is(|x| { x > 0 }, 4); // Is x gt 0 given "4"
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, w);
 
-            // Using higher-order function
-            let w = is(|x| { x > 0 }, 4); // Is x gt 0 given "4"
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, w);
+    // Using a functor
+    let q = functor();
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, q);
 
-            // Using a functor
-            let q = functor();
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, q);
+    // Mondad example
+    let m = monad_example();
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, m);
 
-            // Mondad example
-            let m = monad_example();
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, m);
+    // Function currying
+    let a = not_curried(2, 2);
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, a);
 
-            // Function currying
-            let a = not_curried(2, 2);
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, a);
+    let b = curried(2);
+    let c = b(2);
+    my_object.execute_one(|a| {
+        println!("Introspection: {:#?}", a);
+    }, c);
 
-            let b = curried(2);
-            let c = b(2);
-            my_object.execute_one(|a| {
-                println!("Introspection: {:#?}", a);
-            }, c);
+    // Lazy evaluation print 
+    let l_print = lazy_print();
+    l_print(); // doesn't print until "kicked" or "poked"
 
-            // Lazy evaluation print 
-            let l_print = lazy_print();
-            l_print(); // doesn't print until "kicked" or "poked"
+    // Metaprogramming 
+    print();
+    macro_rule_exe(); 
+    macro_branching();
 
-            // Metaprogramming 
-            print();
-            macro_rule_exe(); 
-            macro_branching();
+    // NOTE: Functional control flow -- see -> rust-simulator
 
-            // NOTE: Functional control flow -- see -> rust-simulator
-        }
-    }
+    // Use data structures
+    let spec = ElevatorSpecification { 
+        floor_count: 1, 
+        floor_height: 0.1, 
+        carriage_weight: 0.2 
+    };
+    let state = ElevatorState { 
+        timestamp: 1.0,
+        location: 1.0,
+        velocity: 1.0,
+        acceleration: 1.0,
+        motor_input: MotorInput::Up { voltage: (3.0) },
+    };
+    let mut motor = SimpleMotorController { esp: spec.clone() };
+    let mut record = SimpleDataRecorder {
+        esp: spec.clone(),
+        record_location: Vec::new(),
+        record_velocity: Vec::new(),
+        record_acceleration: Vec::new(),
+        record_voltage: Vec::new(),
+        
+    };
+    let data = simulate_elevator(
+        spec.clone(), 
+        state.clone(), 
+        FloorRequests::new(), 
+        &mut motor, 
+        &mut record,
+    );
+
+    println!("elevator spec before: {:#?}", spec);
+    println!("elevator spec after: {:#?}", data.esp);
 
     Ok(())
 }
